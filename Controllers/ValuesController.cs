4c7c08d6dd076a181e3252d0483abbe24d0b1905
Michaël Van de Borne <michael.van.de.borne@gmail.com>
2018-05-14 22:41:34 +0200

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace aspnetcoreapi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "deployed", "in", "openshift" };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
