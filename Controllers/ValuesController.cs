7d7177a06bed6e0e7d70349dfb0bdc982e0997e6
Michaël Van de Borne <michael.van.de.borne@gmail.com>
2018-09-06 13:45:50 +0200

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace aspnetcoreapi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "eo", "region" };
+            return new string[] { "hello spacebel", "eo", "regions" };
         }
 
         // GET api/values/5
